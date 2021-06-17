7ef73260c5c808f6a126ff085145aecc519009b8
Dipta panda <diptapanda7@gmail.com>
2021-06-17 20:46:05 +0530

    gods work

---
+++ manthisbetterwork.ts
@@ -0,0 +1,9 @@
+console.log('this files serves no purpose');
+console.log('other then folling the the linguist');
+
+console.log('linguist sucks this project is in ts not html ')
+let r = Math.random().toString(36).substring(7);
+console.log("random", r);
+
+let ra = Math.random().toString(36).substring(7);
+console.log("random", ra);
